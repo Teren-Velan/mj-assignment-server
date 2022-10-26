f22cae82e7ecf714251ba80015ad80714b7b2b88
terenvelan <dev.terenvelan@gmail.com>
2022-10-26 19:54:25 +0800

    enahnce index.js for cors issues

--- index.js
+++ index.js
@@ -21,7 +21,16 @@ mongoose
 const authRoutes = require("./routes/auth.routes");
 const productRoutes = require("./routes/product.routes");
 
-app.use(cors({ origin: "https://mighty-assignment.netlify.app" }));
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use("/uploads", express.static("uploads"));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
