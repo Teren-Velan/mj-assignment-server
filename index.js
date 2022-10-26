2a4a974d8ad2e0bcd3e2a0d91f0b0a22ce8aed7e
terenvelan <dev.terenvelan@gmail.com>
2022-10-26 18:52:39 +0800

    changes for deployment testing

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ const productRoutes = require("./routes/product.routes");
 
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
-app.use(express.static(path.join(__dirname + "/public")));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/product", productRoutes);
