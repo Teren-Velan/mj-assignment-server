68b05e0d6abab943a196220fe359d7f2d3944e15
terenvelan <dev.terenvelan@gmail.com>
2022-10-26 20:18:10 +0800

    ammend variables to allow cors

--- index.js
+++ index.js
@@ -23,11 +23,14 @@ const productRoutes = require("./routes/product.routes");
 
 app.use(cors());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
-  res.header(
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin,X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PATCH,PUT,DELETE,OPTIONS"
   );
   next();
 });
