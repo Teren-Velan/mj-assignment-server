aa86838d0e7e36339442790a872e0b5141addaa7
terenvelan <dev.terenvelan@gmail.com>
2022-10-26 21:32:19 +0800

    modify to allow cors

--- index.js
+++ index.js
@@ -21,17 +21,20 @@ mongoose
 const authRoutes = require("./routes/auth.routes");
 const productRoutes = require("./routes/product.routes");
 
-app.use(cors());
+// app.use(cors());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Origin", "*"); //* to give access to any origin
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization" //to give access to all the headers provided
   );
-  next();
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET"); //to give access to all the methods provided
+    return res.status(200).json({});
+  }
+  console.log({ header: res.header });
+  next(); //so that other routes can take over
 });
 
 app.use("/public/uploads", express.static("uploads"));
