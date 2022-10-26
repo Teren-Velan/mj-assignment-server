61d34491576d7bbdb06d44652ae5f7aed95f0d48
terenvelan <dev.terenvelan@gmail.com>
2022-10-27 01:22:13 +0800

    fixing cors issue

--- index.js
+++ index.js
@@ -21,35 +21,42 @@ mongoose
 const authRoutes = require("./routes/auth.routes");
 const productRoutes = require("./routes/product.routes");
 
-app.use(cors());
+var corsOptions = {
+  origin: [
+    "https://mighty-assignment.netlify.app",
+    "https://mighty-assignment.netlify.app/",
+    "http://localhost:3000",
+    "http://localhost:3000/",
+  ],
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
-app.use(function (req, res, next) {
-  let allowedOrigins = [
-    [
-      "https://mighty-assignment.netlify.app",
-      "https://mighty-assignment.netlify.app/",
-      "http://localhost:3000",
-      "http://localhost:3000/",
-    ],
-  ];
-  let origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin); // restrict it to the required domain
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization" //to give access to all the headers provided
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET"); //to give access to all the methods provided
-    return res.status(200).json({});
-  }
+// app.use(function (req, res, next) {
+//   let allowedOrigins = [
+//     [
+//       "https://mighty-assignment.netlify.app",
+//       "https://mighty-assignment.netlify.app/",
+//       "http://localhost:3000",
+//       "http://localhost:3000/",
+//     ],
+//   ];
+//   let origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//     res.header("Access-Control-Allow-Origin", origin);
+//   }
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+//     return res.status(200).json({});
+//   }
 
-  next(); //so that other routes can take over
-});
+//   next();
+// });
 
-let publicPath = path.join(__dirname, "../public");
-app.use(express.static(publicPath));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/product", productRoutes);
